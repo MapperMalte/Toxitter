f69eef2f09a0e3f94e0feec332bf0c2268fc22f2
Nolden Malte <code@maltenolden.de>
2020-11-19 21:30:07 +0100

    OneToMany etc. etc. works and all

--- src/main/java/Toxitter/Boxfresh/NirvanaQueueSleeper.java
+++ src/main/java/Toxitter/Boxfresh/NirvanaQueueSleeper.java
@@ -6,9 +6,9 @@ import theory.QueueSleeper;
 
 public class NirvanaQueueSleeper<K extends Comparable, V> extends QueueSleeper<K, V>
 {
-
     @Override
-    public void putToSleep(K key, V value) {
+    public void putToSleep(K key, V value)
+    {
 
     }
 
